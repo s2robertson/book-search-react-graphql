985a3eb8d01482d48f8c9b044fe80c4d2b390863
Steven Robertson <s2robertson@gmail.com>
2023-07-08 22:51:00 -0400

    converted login form to use graphql

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,30 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { USER_LOGIN } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [loginUser] = useMutation(USER_LOGIN, {
+    onCompleted({ userLogin: { user, token } }) {
+      console.log(user);
+      Auth.login(token);
+      setUserFormData({
+        email: '',
+        password: '',
+      });
+    },
+    onError(err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+  })
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,26 +41,7 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+    loginUser({ variables: { ...userFormData } });
   };
 
   return (
