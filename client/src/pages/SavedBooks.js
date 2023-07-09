b418569a4804e5ffaafae69548b5042fbdfb2b4f
Steven Robertson <s2robertson@gmail.com>
2023-07-09 17:35:18 -0400

    converted saved books page to delete using graphql

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,77 +6,31 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
-import { useQuery, useApolloClient } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 
-import { deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
-import { QUERY_CURRENT_USER } from '../utils/queries';
+import { QUERY_CURRENT_USER, createCurrentUserCacheUpdater } from '../utils/queries';
+import { DELETE_BOOK } from '../utils/mutations';
+
+const currentUserCacheUpdater = createCurrentUserCacheUpdater('deleteBook')
 
 const SavedBooks = () => {
   // const [userData, setUserData] = useState({});
   const { data, loading } = useQuery(QUERY_CURRENT_USER);
   const userData = data?.currentUser;
 
-  const apolloClient = useApolloClient();
-
-  // use this to determine if `useEffect()` hook needs to run again
-  /*const userDataLength = Object.keys(userData).length;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
+  const [deleteBook] = useMutation(DELETE_BOOK, {
+    onCompleted(data, { variables: { bookId } }) {
+      if (bookId) {
+        removeBookId(bookId);
       }
-    };
-
-    getUserData();
-  }, [userDataLength]); */
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      // this is a temporary hack that will go away once handleDeleteBook uses a mutation
-      apolloClient.writeQuery({
-        query: QUERY_CURRENT_USER,
-        data: { currentUser: updatedUser }
-      });
-      // setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
+    },
+    onError(err) {
       console.error(err);
-    }
-  };
+    },
+    update: currentUserCacheUpdater
+  });
 
   // if data isn't here yet, say so
   if (loading) {
@@ -108,7 +62,7 @@ const SavedBooks = () => {
                     <Card.Title>{book.title}</Card.Title>
                     <p className='small'>Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    <Button className='btn-block btn-danger' onClick={() => deleteBook({ variables: { bookId: book.bookId } })}>
                       Delete this Book!
                     </Button>
                   </Card.Body>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -18,3 +18,11 @@ export const SAVE_BOOK = gql`
         }
     }
 `;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: ID!) {
+        deleteBook(bookId: $bookId) {
+            ...UserDetails
+        }
+    }
+`;
