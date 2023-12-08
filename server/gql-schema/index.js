dfc0e5fe1398b862d4576cd9899ee6a54b7b2c01
Steven Robertson <s2robertson@gmail.com>
2023-12-08 15:27:47 -0500

    converted typeDefs into a .graphql file in preparation for using codegen

--- server/gql-schema/index.js
+++ server/gql-schema/index.js
@@ -1,4 +1,7 @@
-const typeDefs = require('./typeDefs');
+const { readFileSync } = require('fs');
+const path = require('path');
+
+const typeDefs = readFileSync(path.join(__dirname, 'typeDefs.graphql'), { encoding: 'utf-8' });
 const resolvers = require('./resolvers');
 
 module.exports = { typeDefs, resolvers };
