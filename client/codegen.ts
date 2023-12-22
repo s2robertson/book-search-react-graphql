99112c77a398f2ee603652b0b94c86575e94b6f7
Steven Robertson <s2robertson@gmail.com>
2023-12-22 15:00:34 -0500

    disabled fragment masking

--- client/codegen.ts
+++ client/codegen.ts
@@ -2,14 +2,15 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
     schema: '../server/gql-schema/typeDefs.graphql',
-    documents: ['src/utils/*.{ts,js}'],
+    documents: ['src/utils/*.{ts,tsx}'],
     generates: {
         './src/__generated__/': {
             preset: 'client',
             schema: './src/utils/restSchema.graphql',
             plugins: [],
             presetConfig: {
-                gqlTagName: 'gql'
+                gqlTagName: 'gql',
+                fragmentMasking: false
             }
         }
     },
